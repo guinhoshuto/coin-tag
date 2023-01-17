dc86503829c605960746ba4eec09525f7ddca8fd
Guilherme Shuto <guinho.shuto@gmail.com>
2023-01-17 12:02:32 -0400

    webhook novo

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import axios from 'axios'
 import { useEffect } from 'react'
 
-export async function ping(){
-  console.log(import.meta.env.VITE_DISCORD_WEBHOOK)
-  axios.post(
-    import.meta.env.VITE_DISCORD_WEBHOOK!, 
-    { content: 'ping' },
-    { headers: { 'content-type': 'application/json' } }
-  )
-}
+// export async function ping(){
+//   console.log(import.meta.env.VITE_DISCORD_WEBHOOK)
+//   axios.post(
+//     import.meta.env.VITE_DISCORD_WEBHOOK!, 
+//     { content: 'ping' },
+//     { headers: { 'content-type': 'application/json' } }
+//   )
+// }
+axios.post(
+  import.meta.env.VITE_DISCORD_WEBHOOK!, 
+  { content: 'ping' },
+  { headers: { 'content-type': 'application/json' } }
+)
+
 
 function App() {
-  useEffect(() => {
-    ping()
-  }, [])
-''
+  // useEffect(() => {
+  //   ping()
+  // }, [])
   return (
     <div>
       <h1>Teste</h1>
