d456730d7c2d21cc861a339cfebdfd3c639fb131
Guilherme Shuto <guinho.shuto@gmail.com>
2023-01-17 10:17:48 -0400

    ping

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import axios from 'axios'
+import { useEffect } from 'react'
+
+export async function ping(){
+  console.log(import.meta.env.VITE_DISCORD_WEBHOOK)
+  axios.post(
+    import.meta.env.VITE_DISCORD_WEBHOOK!, 
+    { content: 'ping' },
+    { headers: { 'content-type': 'application/json' } }
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  useEffect(() => {
+    ping()
+  }, [])
+''
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h1>Teste</h1>
     </div>
   )
 }
