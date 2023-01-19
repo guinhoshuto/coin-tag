bf0fd1b63dcda6d55d1bd9f22742645857987ab0
Guilherme Shuto <guinho.shuto@gmail.com>
2023-01-19 09:50:25 -0400

    ping datetime

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,24 @@
 import axios from 'axios'
+import { browserVersion, browserName, mobileModel, osName, osVersion} from 'react-device-detect'
+
+
+const content = `
+  **PING** ${new Date().toLocaleString('pt-BR')}
+  **OS**: ${osName} ${osVersion}
+  **Device**: ${mobileModel} 
+  **Browser**: ${browserName} ${browserVersion}
+`
 
 axios.post(
   import.meta.env.VITE_DISCORD_WEBHOOK!, 
-  { content: 'ping' },
+  { content: content },
   { headers: { 'content-type': 'application/json' } }
 )
 
-
 function App() {
   return (
-    <div>
-      <h1>Teste</h1>
+    <div className="app">
+      <h1>teste</h1>
     </div>
   )
 }
